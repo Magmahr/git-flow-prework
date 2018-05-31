d550485e27f5aa841b7979bceaacc7da199c0599
Matt Maher <mattmaher0@gmail.com>
2018-05-31 19:52:07 +0000

    automatically backed up by learn

--- spec/comprehension_spec.rb
+++ spec/comprehension_spec.rb
@@ -270,7 +270,7 @@ describe "GIT" do
     expect(encode(answer)).to eq(encoded_answer)
   end
    
-  it "35. and one developer has pushed to your remote since you cloned it, "
+  it "35. and one developer has pushed to your remote since you cloned it, will 'git status' say you're ahead or behind (or both) origin/master?" do
     # "ahead" or "behind" or "ahead and behind"
     answer = "?"
     encoded_answer = "d81d22fb57ad5589d12d6363f4160b33232c3510"
